c8d91683f5b70dfd460b63800ec2be3827cc373f
stijnblommerde <stijnblommerde@gmail.com>
2016-11-21 16:51:14 +0100

    3 macro

--- hello.py
+++ hello.py
@@ -1,3 +1,13 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
 
 
+@app.route('/comments')
+def comments():
+    comments = ['comment 1', 'comment 2', 'comment 3']
+    return render_template('comments.html', comments=comments)
 
+
+if __name__ == "__main__":
+    app.run(debug=True)
