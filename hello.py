3e1679e7c6fac4e1019bfe6927b663af1c686c45
stijnblommerde <stijnblommerde@tessas-MacBook-Pro-2.local>
2016-11-21 21:40:29 +0100

    3e

--- hello.py
+++ hello.py
@@ -1,20 +1,14 @@
+from datetime import datetime
 from flask import Flask, render_template
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 
 app = Flask(__name__)
 
+manager = Manager(app)
 bootstrap = Bootstrap(app)
-
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-
-@app.route('/user/<name>')
-def user(name):
-    return render_template('user.html', name=name)
+moment = Moment(app)
 
 
 @app.errorhandler(404)
@@ -27,5 +21,19 @@ def page_not_found(e):
     return render_template('500.html'), 500
 
 
+@app.route('/')
+def index():
+    return render_template('index.html',
+                           current_time=datetime.utcnow())
+
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name=name)
+
+
+
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    manager.run()
