aa6c297557f93b1ae067e3d58bed5662b981c7fa
stijnblommerde <stijnblommerde@gmail.com>
2016-11-21 17:44:23 +0100

    2c update

--- hello.py
+++ hello.py
@@ -1,21 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask
 from flask_script import Manager
-from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 
-bootstrap = Bootstrap(app)
+manager = Manager(app)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return '<h1>Hello World!</h1>'
 
 
 @app.route('/user/<name>')
 def user(name):
-    return render_template('user.html', name=name)
+    return '<h1>Hello, %s!</h1>' % name
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    manager.run()
