51a334095295a926c71fd0d2ac175365d55cdcd4
stijnblommerde <stijnblommerde@tessas-MacBook-Pro-2.local>
2016-11-22 10:12:56 +0100

    4a

--- hello.py
+++ hello.py
@@ -3,12 +3,21 @@ from flask import Flask, render_template
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
 
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+
+
+class NameForm(Form):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
 
 
 @app.errorhandler(404)
@@ -21,18 +30,19 @@ def page_not_found(e):
     return render_template('500.html'), 500
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html',
-                           current_time=datetime.utcnow())
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data   # form.name is the form field itself
+        form.name.data = ''     # hiermee clear je het input veld
+    return render_template('index.html', form=form, name=name)
 
 
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name=name)
-
-
-
 
 
 if __name__ == '__main__':
