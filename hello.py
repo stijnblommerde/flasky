a14928a3a95660be149e9968ee3471ec92751c23
stijnblommerde <stijnblommerde@tessas-MacBook-Pro-2.local>
2016-11-24 21:42:57 +0100

    5b

--- hello.py
+++ hello.py
@@ -1,6 +1,4 @@
 import os
-
-from datetime import datetime
 from flask import Flask, render_template, session, url_for, redirect, flash
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
@@ -13,9 +11,8 @@ from flask_sqlalchemy import SQLAlchemy
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
-
 app.config['SECRET_KEY'] = 'hard to guess string'
-app.config['SQLALCHEMY _DATABASE_URI'] = \
+app.config['SQLALCHEMY_DATABASE_URI'] =\
     'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 
@@ -66,12 +63,19 @@ def page_not_found(e):
 def index():
     form = NameForm()
     if form.validate_on_submit():
-        old_name = session.get('name')
-        if old_name is not None and old_name != form.name.data:
-            flash('Looks like you have changed your name!')
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            session['known'] = False
+        else:
+            session['known'] = True
         session['name'] = form.name.data
+        form.name.data = ''
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'))
+    return render_template('index.html',
+                           form=form, name=session.get('name'),
+                           known=session.get('known', False))
 
 
 @app.route('/user/<name>')
