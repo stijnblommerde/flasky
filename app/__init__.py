e9f7c0448e4b49e50c3f7e74c76dacf1945e0fa9
stijnblommerde <stijnblommerde@gmail.com>
2016-11-28 16:29:40 +0100

    8a

--- app/__init__.py
+++ app/__init__.py
@@ -25,7 +25,9 @@ def create_app(config_name):
     # register blueprint
     # .main is a relative import (?)
     from .main import main as main_blueprint
+    from .auth import auth as auth_blueprint
     app.register_blueprint(main_blueprint)
+    app.register_blueprint(auth_blueprint, url_prefix='/auth')
 
     return app
 
--- tests/test_user_model.py
+++ tests/test_user_model.py
@@ -1,25 +1,8 @@
 import unittest
-from flask import current_app
-from app import create_app, db
-from app.models import User, generate_password_hash, check_password_hash
+from app.models import User
 
-class BasicsTestCase(unittest.TestCase):
-    def setUp(self):
-        self.app = create_app('testing')
-        self.app_context = self.app.app_context()
-        self.app_context.push()
-        db.create_all()
 
-    def tearDown(self):
-        db.session.remove()
-        db.drop_all()
-        self.app_context.pop()
-
-    def test_app_exists(self):
-        self.assertFalse(current_app is None)
-
-    def test_app_is_testing(self):
-        self.assertTrue(current_app.config['TESTING'])
+class UserModelTestCase(unittest.TestCase):
 
     def test_password_setter(self):
         stijn = User(password='Kobus1hs')
