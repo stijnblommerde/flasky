8ba0465d22f2561368db590e64b1814954da93c2
stijnblommerde <stijnblommerde@tessas-MacBook-Pro-2.local>
2016-12-09 11:25:16 +0100

    11d

--- config.py
+++ config.py
@@ -15,6 +15,7 @@ class Config:
     FLASKY_MAIL_SENDER = 'stijnblommerde@gmail.com'
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    FLASKY_POSTS_PER_PAGE = 5
 
     @staticmethod
     def init_app(app):
