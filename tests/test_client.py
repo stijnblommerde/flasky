16f68a859270deb1820ddd78827096600cf3a527
stijnblommerde <stijnblommerde@tessas-MacBook-Pro-2.local>
2016-12-13 15:42:09 +0100

    remove environmental variables

---
+++ tests/test_client.py
@@ -0,0 +1,32 @@
+import unittest
+
+from flask import url_for
+
+from app import create_app, db
+
+
+class FlaskClientTestCase(unittest.TestCase):
+    def setUp(self):
+        self.app = create_app('testing')
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        db.create_all()
+        Role.insert_roles()
+        self.client = self.app.test_client(use_cookies=True)
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+        self.app_context.pop()
+
+    def test_home_page(self):
+        # test client
+        response = self.client.get(url_for('main.index'))
+        self.assertTrue('Stranger' in response.get_data(as_text=True))
+
+    def test_register_and_login(self):
+        # register a new account
+        response = self.client.post(url_for('auth.register'), data={
+            'email': 'stijnblommerde@gmail.com',
+            'username': ''
+        })
