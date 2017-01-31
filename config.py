095a244618e707d34e74444340d4c808c6f0041d
stijnblommerde <stijnblommerde@tessas-MacBook-Pro-2.local>
2017-01-31 16:46:16 +0100

    chapter 3 start

--- config.py
+++ config.py
@@ -38,6 +38,7 @@ class DevelopmentConfig(Config):
 
 
 class TestingConfig(Config):
+    print('enter testing config')
     TESTING = True
     SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'data-test.sqlite')
--- vars.sh
+++
@@ -1,4 +0,0 @@
-export FLASKY_CONFIG='default'
-export MAIL_USERNAME='stijnblommerde@gmail.com'
-export MAIL_PASSWORD='JohnLaw41'
-export SECRET_KEY='hard to guess string'
